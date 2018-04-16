f0919516690766b3d67b990141575ce96b16174d
Alina Iuliana Caia <37437952+alinaic@users.noreply.github.com>
2018-04-16 12:27:35 +0200

    Add files via upload

---
+++ Java19javadoc/src/paneles/PanelRegistroProducto.java
@@ -0,0 +1,16 @@
+package paneles;
+
+
+/*comentario regular
+ * 
+ */
+
+/**
+ * 
+ * @author Alina
+ *Clase para mostrar y atender formulario de registro de un producto estandar
+ * para la aplicación
+ */
+public class PanelRegistroProducto {
+
+}
